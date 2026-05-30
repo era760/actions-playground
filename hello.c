3a7c325ffd44d4cd910e9b1b51f19fcaf622a01f
era760 <a21-6229@outlook.jp>
2026-05-30 19:22:11 +0900

    Create C Lang File

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("=====================================\n");
+    printf("hello.c が正常に読み込まれたぞ！\n");
+    printf("Xeon Platinum の上で元気に動いてます！\n");
+    printf("=====================================\n");
+    return 0;
+}
