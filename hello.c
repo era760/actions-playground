b0a02250c37d363f8e78a496327548e61f4c5ec7
era760 <115978105+era760@users.noreply.github.com>
2026-06-03 19:04:13 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <sys/syscall.h>
 
 int main()
 {
-    printf("=====================================\n");
-    printf("hello.c が正常に読み込まれたぞ！\n");
-    printf("Xeon Platinum の上で元気に動いてます！\n");
-    printf("=====================================\n");
+    syscall(1, 1, "A", 1);
     return 0;
 }
