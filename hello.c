5c289dd9a3e0333de201887247ef60bb5758172c
era760 <115978105+era760@users.noreply.github.com>
2026-06-03 19:07:47 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-    putchar('A');
+    putchar('H');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar(',');
+    putchar('W');
+    putchar('o');
+    putchar('r');
+    putchar('l');
+    putchar('d');
+    putchar('!');
     return 0;
 }
